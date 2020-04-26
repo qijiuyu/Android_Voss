9f0a086d10cf55046bccaa8dab752a6cbe12b8df
qijiuyu79 <qijiuyu@bjgoodwill.com>
2020-04-26 21:19:42 +0800

    功能

---
+++ app/src/main/java/com/ylean/android/voss/user/UserActivity.java
@@ -0,0 +1,10 @@
+package com.ylean.android.voss.user;
+
+import com.zxdc.utils.library.base.BaseActivity;
+
+/**
+ * 个人中心
+ * Created by Administrator on 2020/4/26.
+ */
+public class UserActivity extends BaseActivity{
+}
