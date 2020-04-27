3dfeec7cd6e869f3ae9f80cb551a4869fc11b89f
qijiuyu79 <2547863721@qq,com>
2020-04-27 18:18:13 +0800

    修改

---
+++ app/src/main/java/com/ylean/android/voss/user/setting/CertificationActivity.java
@@ -0,0 +1,16 @@
+package com.ylean.android.voss.user.setting;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ylean.android.voss.R;
+import com.zxdc.utils.library.base.BaseActivity;
+
+public class CertificationActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_certification);
+    }
+}
